a65d179f8aa1038894ab9ab0a8033cc0bf690678
Y0h4n3s <yohaksa80@gmail.com>
2021-05-01 15:50:52 -0500

    .

---
+++ helpers/Constants.js
@@ -0,0 +1,22 @@
+module.exports = {
+  framesCount: 20,
+  frames: [
+    { startTime: "08:00", endTime: "08:50" },
+    { startTime: "08:55", endTime: "09:45" },
+    { startTime: "09:50", endTime: "10:40" },
+    { startTime: "10:40", endTime: "11:30" },
+    { startTime: "11:35", endTime: "12:25" },
+    { startTime: "12:30", endTime: "01:30" },
+    { startTime: "01:30", endTime: "02:20" },
+    { startTime: "02:25", endTime: "03:15" },
+    { startTime: "03:20", endTime: "04:10" },
+    { startTime: "04:15", endTime: "05:05" },
+    { startTime: "05:10", endTime: "06:00" },
+    { startTime: "06:05", endTime: "06:55" },
+    { startTime: "07:00", endTime: "07:50" },
+    { startTime: "07:55", endTime: "08:45" },
+    { startTime: "08:50", endTime: "09:40" },
+    { startTime: "09:40", endTime: "10:35" },
+    { startTime: "10:40", endTime: "11:30" },
+  ],
+};
